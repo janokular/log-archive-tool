dc352e78b50cf4afb3738ec92f7f11d107a6de18
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-01-31 12:51:00 +0100

    staged changes

--- log-archive.sh
+++ log-archive.sh
@@ -19,51 +19,45 @@ usage() {
   exit 1
 }
 
-error_only_one_param() {
- echo -e "${RED}Please provide up to one parameter${NC}" >&2
- usage
-}
-
-error_not_a_dir() {
-  echo -e "${RED}Provided parameter is not a directory or does not exist${NC}" >&2
-  usage
-}
-
 # Check if script was run with sudo privliges
 if [[ "${UID}" -ne 0 ]]
 then
-  echo -e "${RED}Pleas run with sudo or as a root${NC}" >&2
+  echo -e "${RED}Please run with sudo or as a root${NC}" >&2
   exit 1
 fi
 
-# Check if user provided directory for archivization
+# Check if user provided LOG_DIR
 if [[ "${#}" -gt 1 ]]
 then
-  error_only_one_param
+  echo -e "${RED}Please provide up to one parameter${NC}" >&2
+  usage
 elif [[ "${#}" -eq 1 ]]
 then
   LOG_DIR="${1}"
 fi
 
-# Check if provided parameter exists and is a directory
-if [[ -d "${LOG_DIR}" ]]
+# Check if LOG_DIR exists and is a directory
+if [[ ! -d "${LOG_DIR}" ]]
 then
-  # Check if ARCHIVE_DIR exists
-  if [[ ! -d "${ARCHIVE_DIR}" ]]
-  then
-    mkdir ${ARCHIVE_DIR}
-  fi
-    tar -zcf ${ARCHIVE_DIR}/${ARCHIVE_FILE} ${LOG_DIR} &> /dev/null
-    
-    # Check to see if the tar command succeeded
-    if [[ "${?}" -ne 0 ]]
-    then
-      echo -e "${RED}The ${LOG_DIR} was NOT archived${NC}" >&2
-      exit 1
-      fi
-    echo "The ${LOG_DIR} was archived as a ${ARCHIVE_FILE}"
+  echo -e "${RED}Provided parameter is not a directory or does not exist${NC}" >&2
+  exit 1
+fi
+
+# Check if ARCHIVE_DIR exists
+if [[ ! -d "${ARCHIVE_DIR}" ]]
+then
+  mkdir ${ARCHIVE_DIR}
+fi
+
+tar -zcf ${ARCHIVE_DIR}/${ARCHIVE_FILE} ${LOG_DIR} &> /dev/null
+
+# Check the status of the tar command
+if [[ "${?}" -ne 0 ]]
+then
+  echo -e "${RED}The ${LOG_DIR} was NOT archived${NC}" >&2
+  exit 1
 else
-  error_not_a_dir
+  echo "The ${LOG_DIR} was archived as a ${ARCHIVE_FILE}"
 fi
 
 exit 0
