72fffa4424632f3a09f73a687c4d36bd74e6a3ab
janokular <janekokularczyk@gmail.com>
2025-04-30 15:24:51 +0200

    staged changes

--- log-archive.sh
+++ log-archive.sh
@@ -9,10 +9,6 @@ DATE=$(date +%Y%m%d)
 TIME=$(date +%H%M%S)
 ARCHIVE_FILE="logs_archive_${DATE}_${TIME}.tar.gz"
 
-# Colors for error messages
-RED='\033[0;31m'
-NC='\033[0m'
-
 usage() {
   echo "Usage: ${0} [LOG_DIR]"
   echo "Archive logs from provided directory: default ${LOG_DIR}"
@@ -22,14 +18,14 @@ usage() {
 # Check if script was run with sudo privileges
 if [[ "${UID}" -ne 0 ]]
 then
-  echo -e "${RED}Please run with sudo or as a root${NC}" >&2
+  echo -e 'Please run with sudo or as a root' >&2
   exit 1
 fi
 
 # Check if user provided LOG_DIR
 if [[ "${#}" -gt 1 ]]
 then
-  echo -e "${RED}Please provide up to one parameter${NC}" >&2
+  echo -e 'Please provide up to one parameter' >&2
   usage
 elif [[ "${#}" -eq 1 ]]
 then
@@ -39,7 +35,7 @@ fi
 # Check if LOG_DIR exists and is a directory
 if [[ ! -d "${LOG_DIR}" ]]
 then
-  echo -e "${RED}Provided parameter is not a directory or does not exist${NC}" >&2
+  echo -e 'Provided parameter is not a directory or does not exist' >&2
   exit 1
 fi
 
@@ -54,7 +50,7 @@ tar -zcf ${ARCHIVE_DIR}/${ARCHIVE_FILE} ${LOG_DIR} &> /dev/null
 # Check the status of the tar command
 if [[ "${?}" -ne 0 ]]
 then
-  echo -e "${RED}The ${LOG_DIR} was NOT archived${NC}" >&2
+  echo -e "The ${LOG_DIR} was NOT archived" >&2
   exit 1
 else
   echo "The ${LOG_DIR} was archived as a ${ARCHIVE_FILE}"
