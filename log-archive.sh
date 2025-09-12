9758d2517ffd2c0c7fc2093cde4c4dc7cc230c55
janokular <janekokularczyk@gmail.com>
2025-09-12 15:15:08 +0200

    updated log-archive.sh

--- log-archive.sh
+++ log-archive.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This script archives logs
-# By compressing them and storing them in a new directory
+# This script archives logs by compressing them and storing them in a new directory
 
 LOG_DIR='/var/log'
 ARCHIVE_DIR="${HOME}/archive"
@@ -18,14 +17,14 @@ usage() {
 # Check if script was run with sudo privileges
 if [[ "${UID}" -ne 0 ]]
 then
-  echo -e 'Please run with sudo or as a root' >&2
+  echo 'Please run with sudo or as a root' >&2
   exit 1
 fi
 
 # Check if user provided LOG_DIR
 if [[ "${#}" -gt 1 ]]
 then
-  echo -e 'Please provide up to one parameter' >&2
+  echo 'Please provide up to one parameter' >&2
   usage
 elif [[ "${#}" -eq 1 ]]
 then
@@ -35,7 +34,7 @@ fi
 # Check if LOG_DIR exists and is a directory
 if [[ ! -d "${LOG_DIR}" ]]
 then
-  echo -e 'Provided parameter is not a directory or does not exist' >&2
+  echo 'Provided parameter is not a directory or does not exist' >&2
   exit 1
 fi
 
@@ -50,7 +49,7 @@ tar -zcf ${ARCHIVE_DIR}/${ARCHIVE_FILE} ${LOG_DIR} &> /dev/null
 # Check the status of the tar command
 if [[ "${?}" -ne 0 ]]
 then
-  echo -e "The ${LOG_DIR} was NOT archived" >&2
+  echo "The ${LOG_DIR} was NOT archived" >&2
   exit 1
 else
   echo "The ${LOG_DIR} was archived as a ${ARCHIVE_FILE}"
