f9e69a1c6d25fcb8ca423485a1d3998e9e6356ab
Jan <janekokularczyk@gmail.com>
2025-10-23 19:29:54 +0200

    refactored

--- log-archive.sh
+++ log-archive.sh
@@ -2,56 +2,51 @@
 
 # This script archives logs by compressing them and storing them in a new directory
 
-LOG_DIR='/var/log'
-ARCHIVE_DIR="${HOME}/archive"
-DATE=$(date +%Y%m%d)
-TIME=$(date +%H%M%S)
-ARCHIVE_FILE="logs_archive_${DATE}_${TIME}.tar.gz"
+log_dir='/var/log'
+archive_dir="${HOME}/archive"
+date=$(date +%Y%m%d)
+time=$(date +%H%M%S)
+archive_file="logs_archive_${date}_${time}.tar.gz"
 
 usage() {
   echo "Usage: ${0} [LOG_DIR]"
-  echo "Archive logs from provided directory: default ${LOG_DIR}"
+  echo "Archive logs from provided directory: default ${log_dir}"
   exit 1
 }
 
 # Check if script was run with sudo privileges
-if [[ "${UID}" -ne 0 ]]
-then
+if [[ "${UID}" -ne 0 ]]; then
   echo 'Please run with sudo or as a root' >&2
   exit 1
 fi
 
-# Check if user provided LOG_DIR
-if [[ "${#}" -gt 1 ]]
-then
+# Check if user provided log_dir
+if [[ "${#}" -gt 1 ]]; then
   echo 'Please provide up to one parameter' >&2
   usage
 elif [[ "${#}" -eq 1 ]]
 then
-  LOG_DIR="${1}"
+  log_dir="${1}"
 fi
 
-# Check if LOG_DIR exists and is a directory
-if [[ ! -d "${LOG_DIR}" ]]
-then
+# Check if log_dir exists and is a directory
+if [[ ! -d "${log_dir}" ]]; then
   echo 'Provided parameter is not a directory or does not exist' >&2
   exit 1
 fi
 
-# Check if ARCHIVE_DIR exists
-if [[ ! -d "${ARCHIVE_DIR}" ]]
-then
-  mkdir ${ARCHIVE_DIR}
+# Check if archive_dir exists
+if [[ ! -d "${archive_dir}" ]]; then
+  mkdir ${archive_dir}
 fi
 
-tar -zcf ${ARCHIVE_DIR}/${ARCHIVE_FILE} ${LOG_DIR} &> /dev/null
+tar -zcf ${archive_dir}/${archive_file} ${log_dir} &> /dev/null
 
 # Check the status of the tar command
-if [[ "${?}" -ne 0 ]]
-then
-  echo "The ${LOG_DIR} was NOT archived" >&2
+if [[ "${?}" -ne 0 ]]; then
+  echo "The ${log_dir} was NOT archived" >&2
   exit 1
 else
-  echo "The ${LOG_DIR} was archived as a ${ARCHIVE_FILE}"
+  echo "The ${log_dir} was archived as a ${archive_file}"
   exit 0
 fi
